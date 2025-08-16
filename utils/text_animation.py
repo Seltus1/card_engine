9a897e2cd722842936ea28eb98602390a8ded567
DanyDiab <danydiab04@gmail.com>
2025-08-16 01:56:16 -0300

    can now chain animations

--- utils/text_animation.py
+++ utils/text_animation.py
@@ -4,58 +4,59 @@ from rich import print
 import os
 import time
 
-def stringColorChange(text: str, colors: list[str], swapTime, totalTime):
+def stringColorChange(live: Live, text: str, colors: list[str], swapTime, totalTime):
     styled = text2art(text,font="block")
     startTime = time.time()
     lastFlashTime = 0
     colorIndex = 0
-    with Live(refresh_per_second=60) as live:
-        while(True):
-            currTime = time.time()
+    while(True):
+        currTime = time.time()
+        live.update(f"[{colors[colorIndex]}]{styled}[/{colors[colorIndex]}]")
+        if currTime - startTime > totalTime: break
+        if (currTime - lastFlashTime) > swapTime:
             live.update(f"[{colors[colorIndex]}]{styled}[/{colors[colorIndex]}]")
-            if currTime - startTime > totalTime: break
-            if (currTime - lastFlashTime) > swapTime:
-                live.update(f"[{colors[colorIndex]}]{styled}[/{colors[colorIndex]}]")
-                colorIndex+=1
-                colorIndex %= len(colors)
-                lastFlashTime = time.time()
+            colorIndex+=1
+            colorIndex %= len(colors)
+            lastFlashTime = time.time()
             
-def stringWave(text: str, colors: list[str], swapTime, loops):
+def stringWave(live:Live, text: str, colors: list[str], swapTime, loops):
     styledChars = [text2art(letter, font="block").splitlines() for letter in text]
     flashingIndex = -1
     lastFlashTime = 0
     currLoop = 0
     incrementer = 1
-    with Live(refresh_per_second=60) as live:
-        while True:
-            if time.time() - lastFlashTime > swapTime:
-                flashingIndex += incrementer
-                lastFlashTime = time.time()
+    while True:
+        if time.time() - lastFlashTime > swapTime:
+            flashingIndex += incrementer
+            lastFlashTime = time.time()
+        
+        if flashingIndex >= len(styledChars) or flashingIndex < 0:
+            incrementer *= -1
+            currLoop += 1
+            flashingIndex += incrementer
+            if currLoop == loops:
+                break
             
-            if flashingIndex >= len(styledChars) or flashingIndex < 0:
-                incrementer *= -1
-                currLoop += 1
-                flashingIndex += incrementer
-                if currLoop == loops:
-                    break
-            
-            fullStringLines = []
-            for i in range(len(styledChars[0])):
-                line = ""
-                for j in range(len(styledChars)):
-                    char_lines = styledChars[j]
-                    if j == flashingIndex:
-                        line += f"[{colors[1]}]{char_lines[i]}[/{colors[1]}]"
-                    else:
-                        line += f"[{colors[0]}]{char_lines[i]}[/{colors[0]}]"
-                fullStringLines.append(line)
-            
-            fullString = "\n".join(fullStringLines)
-            live.update(fullString)
+        fullStringLines = []
+        for i in range(len(styledChars[0])):
+            line = ""
+            for j in range(len(styledChars)):
+                char_lines = styledChars[j]
+                if j == flashingIndex:
+                    line += f"[{colors[1]}]{char_lines[i]}[/{colors[1]}]"
+                else:
+                    line += f"[{colors[0]}]{char_lines[i]}[/{colors[0]}]"
+            fullStringLines.append(line)
+        
+        fullString = "\n".join(fullStringLines)
+        live.update(fullString)
 
 
 
 
 if __name__ == "__main__":
     text = "Hi Stimmer"
-    stringWave(text,["green","white"], .2, 4)
+    with Live(refresh_per_second=60) as live:
+        stringColorChange(live, text,["green","white"], .2,2)
+        stringWave(live, text,["green","black"], .2, 2)
+        stringColorChange(live, text,["red","yellow"], .2,2)
